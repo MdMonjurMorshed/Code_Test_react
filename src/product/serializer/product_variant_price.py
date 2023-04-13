85e246c58afd38c1592db43babcddfbc31fcbe98
MD MONJUR MORSHED <morshed.naptechlabs@gmail.com>
2023-04-13 16:05:18 +0600

    merge

--- src/product/serializer/product_variant_price.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from product.models import *
-
-class ProductVariantPrice_selializer(serializers.ModelSerializer):
-    class Meta:
-        model=ProductVariantPrice
-        fields="__all__"
